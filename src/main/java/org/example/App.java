e6a95afe7b7a8f5a7347c943696a9a59a4962fdf
Luis Curtiellas <curtiellas98@gmail.com>
2021-09-11 09:07:50 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 luis curtiellas
+ */
+
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        //assume one gallon covers 350 square feet
+        final float coverage = 350;
+
+        //read length
+        System.out.println("What is the length of the room in feet?");
+        float length = sc.nextFloat();
+
+        //read width
+        System.out.println("What is the width of the room in feet?");
+        float width = sc.nextFloat();
+
+        //calculate
+        float area = length * width;
+        double gallonsNeeded = Math.ceil(area / coverage);
+
+        System.out.printf( "You will need to purchase %.0f gallons of paint to cover %.0f square feet.", gallonsNeeded, area );
+    }
+}
